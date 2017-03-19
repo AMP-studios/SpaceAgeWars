3b519ce666ad3ecb630d5e65371af709a3d1e278
LeafyLen <agrebenik@live.com>
2017-03-18 21:08:14 -0700

    added TestZone to gitignore

--- src/main/java/SpaceAgeWars/Core/Main.java
+++ src/main/java/SpaceAgeWars/Core/Main.java
@@ -1,5 +1,6 @@
 package SpaceAgeWars.Core;
 
+
 public class Main {
 	public static void main(String[] args) {
 		//todo
---
+++ src/main/java/SpaceAgeWars/Core/TestZone.java
@@ -0,0 +1,7 @@
+package SpaceAgeWars.Core;
+
+/**
+ * Created by bodyi on 3/18/2017.
+ */
+public class TestZone {
+}
