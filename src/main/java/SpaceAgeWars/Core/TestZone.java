23de5e4a2f209e918348c2d0d5420673f022d544
LeafyLen <agrebenik@live.com>
2017-03-18 22:23:03 -0700

    added gluegen to .gitignore

--- src/main/java/SpaceAgeWars/Core/TestZone.java
+++ src/main/java/SpaceAgeWars/Core/TestZone.java
@@ -1,9 +1,90 @@
 package SpaceAgeWars.Core;
 
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.FPSAnimator;
+
+import static com.jogamp.opengl.GL.*;
+import static com.jogamp.opengl.GL2ES1.GL_PERSPECTIVE_CORRECTION_HINT;
+import static com.jogamp.opengl.fixedfunc.GLLightingFunc.GL_SMOOTH;
+import static com.jogamp.opengl.fixedfunc.GLMatrixFunc.GL_MODELVIEW;
+import static com.jogamp.opengl.fixedfunc.GLMatrixFunc.GL_PROJECTION;
+
 /**
  * Created by bodyi on 3/18/2017.
  */
-public class TestZone {
+@SuppressWarnings("serial")
+public class TestZone extends GLCanvas implements GLEventListener {
+	private static final String TITLE = "TestZone ^-^";
+	public static final int SCREEN_WIDTH = 100;
+	public static final int SCREEN_HEIGHT = 100;
+	GLCanvas canvas = new GLCanvas();
+	FPSAnimator animator = new FPSAnimator(canvas,60);
+	private GLU glu;
+
+
+	public TestZone() {
+		this.addGLEventListener(this);
+	}
+
+	@Override
+	public void init(GLAutoDrawable drawable) {
+		GL2 gl = drawable.getGL().getGL2();      // get the OpenGL graphics context
+		glu = new GLU();                         // get GL Utilities
+		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // set background (clear) color
+		gl.glClearDepth(1.0f);      // set clear depth value to farthest
+		gl.glEnable(GL_DEPTH_TEST); // enables depth testing
+		gl.glDepthFunc(GL_LEQUAL);  // the type of depth test to do
+		gl.glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); // best perspective correction
+		gl.glShadeModel(GL_SMOOTH); // blends colors nicely, and smoothes out lighting
+
+		// ----- Your OpenGL initialization code here -----
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable glAutoDrawable) {
+
+	}
+
+
+	@Override
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+		GL2 gl = drawable.getGL().getGL2();  // get the OpenGL 2 graphics context
+
+		if (height == 0) height = 1;   // prevent divide by zero
+		float aspect = (float)width / height;
+
+		// Set the view port (display area) to cover the entire window
+		gl.glViewport(0, 0, width, height);
+
+		// Setup perspective projection, with aspect ratio matches viewport
+		gl.glMatrixMode(GL_PROJECTION);  // choose projection matrix
+		gl.glLoadIdentity();             // reset projection matrix
+		glu.gluPerspective(45.0, aspect, 0.1, 100.0); // fovy, aspect, zNear, zFar
+
+		// Enable the model-view transform
+		gl.glMatrixMode(GL_MODELVIEW);
+		gl.glLoadIdentity(); // reset
+	}
+
+	@Override
+	public void display(GLAutoDrawable drawable) {
+		GL2 gl = drawable.getGL().getGL2();  // get the OpenGL 2 graphics context
+		gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear color and depth buffers
+		gl.glLoadIdentity();  // reset the model-view matrix
+
+		// ----- Your OpenGL rendering code here (Render a white triangle for testing) -----
+		gl.glTranslatef(0.0f, 0.0f, -6.0f); // translate into the screen
+		gl.glBegin(GL_TRIANGLES); // draw using triangles
+		gl.glVertex3f(0.0f, 1.0f, 0.0f);
+		gl.glVertex3f(-1.0f, -1.0f, 0.0f);
+		gl.glVertex3f(1.0f, -1.0f, 0.0f);
+		gl.glEnd();
+	}
 
 
 }
+
