8e5b1b394a989aed6e930bcc24fddbcc0376ccd7
Pavel Braginskiy <pavelyay@gmail.com>
2017-03-18 23:04:39 -0700

    Started making AudioController.

--- src/main/java/SpaceAgeWars/CustomUtils/AudioController.java
+++ src/main/java/SpaceAgeWars/CustomUtils/AudioController.java
@@ -2,11 +2,20 @@ package SpaceAgeWars.CustomUtils;
 
 import com.jogamp.openal.AL;
 import com.jogamp.openal.ALFactory;
+import com.jogamp.openal.util.WAVData;
+import com.jogamp.openal.util.WAVLoader;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 @SuppressWarnings("unused")
 public class AudioController {
 	private AL al = ALFactory.getAL();
-	private int[] buffer = new int[1], source = new int[1];
+	private HashMap<String, WAVData> sounds = new HashMap<String, WAVData>();
 
-
+	public boolean addSound(String name, String path) throws IOException {
+		if (sounds.containsKey(name)) return false;
+		sounds.put(name, WAVLoader.loadFromFile(path));
+		return true;
+	}
 }
